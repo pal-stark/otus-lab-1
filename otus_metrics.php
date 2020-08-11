0b96e650cdd4db0ac935e49b91fb58a01557bfef
Pavel V. Strokov <pavel.strokov@gmail.com>
2020-08-11 22:02:30 +0300

    Результвт работы. Signed-off-by:Pavel V. Strokov <pavel.strokov@gmail.com>

---
+++ otus_metrics.php
@@ -0,0 +1,35 @@
+#!/usr/bin/php
+<?php
+/*
+ *
+ */
+
+//remove script name
+array_shift($argv);
+$cmd    = trim(array_shift($argv));
+$param1 = trim(array_shift($argv));
+
+//number of metrics
+$n   = 3;
+//metrics key for discovery
+$mk  = '{#METRICNAME}';
+$mp  = 'metric';
+$out = '';
+
+switch ($cmd) {
+    //Get value for any metric
+    case 'value':
+        $out  = rand(0, 100);
+        break;
+    //Get list of metrics
+    case 'discovery':
+    default:
+        $data = array();
+        for ($i = 0; $i < $n;) {
+            $m        = new stdClass();
+            $m->{$mk} = $mp . ++$i;
+            $data[]   = $m;
+        }
+        $out = json_encode($data);
+}
+echo $out;
